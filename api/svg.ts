f0ada23e6772a2bc30ae6039ff598503daa2d7ed
ci7lus <7887955+ci7lus@users.noreply.github.com>
2025-12-11 22:20:15 +0900

    chore: Biomeを導入し、pnpmへ移行、ビルド設定と型定義ファイルを更新し、nodemonとyarnを削除

--- api/svg.ts
+++ api/svg.ts
@@ -1,100 +1,104 @@
-import querystring from "querystring"
-import isUrl from "is-url"
-import axios from "axios"
-import domino from "domino"
-import { detectEncode } from "../src/encoding"
-import { getMetadata } from "page-metadata-parser"
-import iconv from "iconv-lite"
-import { generateSvg } from "../src/image/ogpSvg"
-import fs from "fs"
-import sharp from "sharp"
-import type { VercelRequest, VercelResponse } from "@vercel/node"
+import fs from "node:fs";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import axios from "axios";
+import domino from "domino";
+import iconv from "iconv-lite";
+import isUrl from "is-url";
+import { getMetadata } from "page-metadata-parser";
+import sharp from "sharp";
+import { detectEncode } from "../src/encoding";
+import { generateSvg } from "../src/image/ogpSvg";
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  res.setHeader("access-control-allow-origin", "*")
-  if (req.method === "OPTIONS") {
-    res.setHeader("access-control-allow-methods", "GET, OPTIONS")
-    res.setHeader("access-control-allow-headers", "*")
-    res.setHeader("access-control-max-age", "86400")
-    res.statusCode = 204
-    res.end()
-    return
-  }
-  const url = req.query.url
-  if (typeof url !== "string") {
-    return res.status(500).json({ message: "url parse error" })
-  }
-  if (!isUrl(url)) {
-    return res.status(400).json({ message: "requested url is not valid" })
-  }
-  const borderMode = req.query.border !== "no"
+	res.setHeader("access-control-allow-origin", "*");
+	if (req.method === "OPTIONS") {
+		res.setHeader("access-control-allow-methods", "GET, OPTIONS");
+		res.setHeader("access-control-allow-headers", "*");
+		res.setHeader("access-control-max-age", "86400");
+		res.statusCode = 204;
+		res.end();
+		return;
+	}
+	const url = req.query.url;
+	if (typeof url !== "string") {
+		return res.status(500).json({ message: "url parse error" });
+	}
+	if (!isUrl(url)) {
+		return res.status(400).json({ message: "requested url is not valid" });
+	}
+	const borderMode = req.query.border !== "no";
 
-  const r = await axios.get<Buffer>(encodeURI(url), {
-    headers: { "User-Agent": "Twitterbot/1.0" },
-    responseType: "arraybuffer",
-  })
-  if (r.status !== 200) {
-    return res
-      .status(r.status)
-      .json({ message: `remote status code was ${r.status}` })
-  }
-  if (![r.headers["content-type"]].flat().shift()?.startsWith("text/html")) {
-    return res.status(400).json({ message: "remote content was not html" })
-  }
-  const buf = r.data
+	const r = await axios.get<Buffer>(encodeURI(url), {
+		headers: { "User-Agent": "Twitterbot/1.0" },
+		responseType: "arraybuffer",
+		validateStatus: () => true,
+	});
+	if (r.status !== 200) {
+		return res
+			.status(r.status)
+			.json({ message: `remote status code was ${r.status}` });
+	}
+	if (![r.headers["content-type"]].flat().shift()?.startsWith("text/html")) {
+		return res.status(400).json({ message: "remote content was not html" });
+	}
+	const buf = r.data;
 
-  let html: string
-  const encoding = detectEncode(buf)
-  if (encoding) {
-    html = iconv.decode(buf, encoding)
-  } else {
-    html = buf.toString("ascii")
-  }
+	let html: string;
+	const encoding = detectEncode(buf);
+	if (encoding) {
+		html = iconv.decode(buf, encoding);
+	} else {
+		html = buf.toString("ascii");
+	}
 
-  let style: string | null = null
-  try {
-    style = await fs.promises.readFile("./svg.tailwind.css", "utf8")
-  } catch (error) {
-    console.error(error)
-  }
-  if (!style) {
-    return res.status(500).json({ message: "style load failed" })
-  }
+	let style: string | null = null;
+	try {
+		style = await fs.promises.readFile("./svg.tailwind.css", "utf8");
+	} catch (error) {
+		console.error(error);
+	}
+	if (!style) {
+		return res.status(500).json({ message: "style load failed" });
+	}
 
-  try {
-    const { document } = domino.createWindow(html)
-    const responseUrl = r.request.res.responseUrl || url
-    const metadata = getMetadata(document, responseUrl)
-    const iconUrl = metadata.image || metadata.icon
-    let icon: string | undefined
-    try {
-      if (iconUrl) {
-        const r = await axios.get<Buffer>(iconUrl, {
-          headers: {
-            "User-Agent":
-              "ricapitolare (+https://github.com/ci7lus/ricapitolare)",
-          },
-          responseType: "arraybuffer",
-        })
-        const mime = [r.headers["content-type"]].flat().shift()?.toLowerCase()
-        const buff = await sharp(r.data).resize(null, 256).toBuffer()
-        icon = `data:${mime};base64,` + buff.toString("base64")
-      }
-    } catch (error) {
-      console.error(error)
-    }
-    const svg = generateSvg({
-      style: style.replace(/\<.+\>/g, ""),
-      ...metadata,
-      icon,
-      borderMode,
-      url: responseUrl,
-    })
-    res.setHeader("content-type", "image/svg+xml")
-    res.setHeader("cache-control", "s-maxage=3600, stale-while-revalidate")
-    return res.status(200).send(svg).end()
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({ message: "parse error" })
-  }
+	try {
+		const { document } = domino.createWindow(html);
+		const responseUrl = r.request.res.responseUrl || url;
+		const metadata = getMetadata(document, responseUrl) as {
+			title: string;
+			image: string;
+			icon: string;
+		};
+		const iconUrl = metadata.image || metadata.icon;
+		let icon: string | undefined;
+		try {
+			if (iconUrl) {
+				const r = await axios.get<Buffer>(iconUrl, {
+					headers: {
+						"User-Agent":
+							"ricapitolare (+https://github.com/ci7lus/ricapitolare)",
+					},
+					responseType: "arraybuffer",
+				});
+				const mime = [r.headers["content-type"]].flat().shift()?.toLowerCase();
+				const buff = await sharp(r.data).resize(null, 256).toBuffer();
+				icon = `data:${mime};base64,${buff.toString("base64")}`;
+			}
+		} catch (error) {
+			console.error(error);
+		}
+		const svg = generateSvg({
+			style: style.replace(/<.+>/g, ""),
+			...metadata,
+			icon,
+			borderMode,
+			url: responseUrl,
+		});
+		res.setHeader("content-type", "image/svg+xml");
+		res.setHeader("cache-control", "s-maxage=3600, stale-while-revalidate");
+		return res.status(200).send(svg).end();
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: "parse error" });
+	}
 }
--- src/image/ogpSvg.ts
+++ src/image/ogpSvg.ts
@@ -1,45 +1,45 @@
-import xss from "xss"
+import xss from "xss";
 
 export const generateSvg = (options: {
-  style: string
-  icon?: string
-  title?: string
-  url: string
-  description?: string
-  provider?: string
-  borderMode: boolean
+	style: string;
+	icon?: string;
+	title?: string;
+	url: string;
+	description?: string;
+	provider?: string;
+	borderMode: boolean;
 }) => {
-  const style = options.style
-  const icon = options.icon && xss(options.icon)
-  const title = options.title && xss(options.title)
-  const url = xss(options.url)
-  const description = options.description && xss(options.description)
-  const provider = options.provider && xss(options.provider)
-  const borderMode = options.borderMode
-  return `
+	const style = options.style;
+	const icon = options.icon && xss(options.icon);
+	const title = options.title && xss(options.title);
+	const url = xss(options.url);
+	const description = options.description && xss(options.description);
+	const provider = options.provider && xss(options.provider);
+	const borderMode = options.borderMode;
+	return `
 <svg xmlns='http://www.w3.org/2000/svg' width='640px' height='144px'>
     <foreignObject width='640px' height='144px' requiredExtensions="http://www.w3.org/1999/xhtml">
-        <style>${style}</style>
+        <style><![CDATA[${style}]]></style>
         <body xmlns="http://www.w3.org/1999/xhtml" class="h-full w-full">
             <div class="antialiased font-sans bg-white dark:bg-gray-900 dark:text-gray-100 w-full h-full p-4 ${
-              borderMode ? "rounded-md" : ""
-            } border-gray-800 border flex">
+							borderMode ? "rounded-md" : ""
+						} border-gray-800 border flex">
                 <div class="flex-shrink-0 flex items-center justify-center pl-4 pr-8">
                     <div class="bg-cover bg-center w-16 h-16 border rounded-md border-gray-600 border-opacity-50" style="background-image: url(${icon})" />
                 </div>
                 <div class="flex-grow w-64 flex flex-col justify-center space-y-1">
                     <h1 class="truncate text-xl">${title || ""}</h1>
                     <p class="truncate text-gray-800 dark:text-gray-200">${
-                      description || ""
-                    }</p>
+											description || ""
+										}</p>
                     <p class="truncate text-sm text-gray-600 dark:text-gray-400">${
-                      provider || ""
-                    }</p>
+											provider || ""
+										}</p>
                     <a class="truncate text-sm text-blue-400 hover:underline block" href="${url}" target="_blank">${url}</a>
                 </div>
             </div>
         </body>
     </foreignObject>
 </svg>
-`.trim()
-}
+`.trim();
+};
