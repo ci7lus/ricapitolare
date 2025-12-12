f78aa61ba89b59bb1903e038b0bcd43856b415aa
ci7lus <7887955+ci7lus@users.noreply.github.com>
2025-12-12 23:19:41 +0900

    fix: svgレイアウト2

--- src/image/ogpSvg.ts
+++ src/image/ogpSvg.ts
@@ -21,11 +21,11 @@ export const generateSvg = (options: {
     <foreignObject width='640px' height='144px' requiredExtensions="http://www.w3.org/1999/xhtml">
         <style><![CDATA[${style}]]></style>
         <body xmlns="http://www.w3.org/1999/xhtml" class="h-full w-full">
-            <div class="antialiased font-sans bg-white dark:bg-gray-900 dark:text-gray-100 w-full h-full py-4 px-2 ${borderMode ? "rounded-md" : ""} border-gray-800 border flex">
-                <div class="flex-shrink-0 flex items-center justify-center pl-4">
+            <div class="antialiased font-sans bg-white dark:bg-gray-900 dark:text-gray-100 w-full h-full py-4 px-6 ${borderMode ? "rounded-md" : ""} border-gray-800 border flex">
+                <div class="flex-shrink-0 flex items-center justify-center pr-6">
                     <div class="bg-cover bg-center w-16 h-16 border rounded-md border-gray-600 border-opacity-50" style="background-image: url(${icon})" />
                 </div>
-                <div class="flex-grow w-64 flex flex-col justify-center space-y-1 px-4">
+                <div class="flex-grow w-64 flex flex-col justify-center space-y-1">
                     <h1 class="truncate text-xl">${title}</h1>
                     <p class="truncate text-gray-800 dark:text-gray-200">${description}</p>
                     <p class="truncate text-sm text-gray-600 dark:text-gray-400">${provider}</p>
